fb3ccbabbc76ff765c651d90d8108938f605f29f
edCoder1 <edwin.lsb1@gmail.com>
2022-03-27 11:56:46 -0600

    Portfolio section version 0.8

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,26 @@ import './portfolio.scss';
 export default function Portfolio() {
   return (
     <div id='portfolio' className='portfolio'>
-      Portfolio
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/coding.jpg"/>
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/coding.jpg"/>
+          <h3>Web App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/coding.jpg"/>
+          <h3>Mobile App</h3>
+        </div>
+      </div>
     </div>
   )
 }
