45cd53dd62607aac2fd25d90d3adf591119ec6db
edCoder1 <edwin.lsb1@gmail.com>
2022-03-27 12:30:55 -0600

    Portfolio section version 0.9 - added menu functionality and portfolioList

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,13 +1,30 @@
 import './portfolio.scss';
 
+import PortfolioList from './portfolio-list/PortfolioList'
+import { useState } from 'react';
+
+
 export default function Portfolio() {
+  const [slected, setSelected] = useState('featured');
+
+  const portfolioList = [
+    { id: 'featured', title: 'Featured' },
+    { id: 'web',      title: 'Web Application' },
+    { id: 'mobile',   title: 'Moblie Application' },
+  ];
+
   return (
     <div id='portfolio' className='portfolio'>
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">Featured</li>
-        <li>Web App</li>
-        <li>Mobile App</li>
+        {portfolioList.map((item) =>
+          <PortfolioList
+            id={item.id}
+            title={item.title}
+            active={slected === item.id}
+            setSelected={setSelected}
+          />
+        )}
       </ul>
       <div className="container">
         <div className="item">
